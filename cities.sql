5000e22d5bba059cf389bae56ab791e0432c5d40
Theekshana Kasthuriarachchi <theekshanattk@gmail.com>
2023-08-01 09:31:17 +0530

    Create cities.sql

---
+++ cities.sql
@@ -0,0 +1,17 @@
+
+--
+-- Table structure for table `cities` | @theekshana
+--
+
+CREATE TABLE cities (
+	id bigserial NOT NULL,
+	district_id int4 NOT NULL,
+	name varchar(255) NOT NULL,
+	CONSTRAINT cities_pkey PRIMARY KEY (id)
+);
+
+
+-- cities foreign keys
+
+ALTER TABLE cities ADD CONSTRAINT fk_district FOREIGN KEY (district_id) REFERENCES districts(id);
+
